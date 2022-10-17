3839863b07cec52c38f02e55beb633069306b481
Zone <2493920954@qq.com>
2022-10-17 19:05:32 +0800

    四个模块

--- app/src/main/java/com/zone/pictureeditor/MainActivity.kt
+++ app/src/main/java/com/zone/pictureeditor/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle.Companion.Italic
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -96,12 +97,27 @@ fun EditCard() {
             .fillMaxWidth()
             .fillMaxHeight(0.5f)
             .padding(10.dp),
-        elevation = 8.dp,
+        elevation = 6.dp,
         shape = RoundedCornerShape(10.dp)
     ) {
-//                Icon(imageVector = Icons.Default.Build, contentDescription = "Settings")
-        Text(text = "Edit",
-            modifier = Modifier.background(Color(194, 204, 208)).wrapContentSize())
+        Column(
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(top = 30.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Default.MailOutline,
+                contentDescription = "Edit",
+                tint = Color(22, 133, 169),
+                modifier = Modifier.width(60.dp).height(60.dp)
+            )
+            Text(
+                text = "Edit",
+                fontSize = 20.sp,
+                fontFamily = FontFamily.Serif,
+                modifier = Modifier.wrapContentSize().padding(top = 10.dp)
+            )
+        }
     }
 }
 
@@ -111,12 +127,28 @@ fun ConvertToPDFCard() {
         Modifier
             .fillMaxSize()
             .padding(10.dp),
-        elevation = 8.dp,
+        elevation = 6.dp,
         shape = RoundedCornerShape(10.dp)
     ) {
-//                Icon(imageVector = Icons.Default.Info, contentDescription = "Convert To PDF")
-        Text(text = "Convert To PDF",
-            modifier = Modifier.background(Color(194, 204, 208)).wrapContentSize())
+        Column(
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(top = 30.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Default.Face,
+                contentDescription = "Convert To PDF",
+                tint = Color(22, 133, 169),
+                modifier = Modifier.width(60.dp).height(60.dp)
+            )
+            Text(
+                text = "Convert To\n PDF",
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center,
+                fontFamily = FontFamily.Serif,
+                modifier = Modifier.wrapContentSize().padding(top = 10.dp)
+            )
+        }
     }
 }
 
@@ -127,12 +159,27 @@ fun Card3() {
             .fillMaxWidth()
             .fillMaxHeight(0.5f)
             .padding(10.dp),
-        elevation = 8.dp,
+        elevation = 6.dp,
         shape = RoundedCornerShape(10.dp)
     ) {
-//                Icon(imageVector = Icons.Default.Check, contentDescription = "card3")
-        Text(text = "card3(暂时不知道要叫什么)",
-            modifier = Modifier.background(Color(194, 204, 208)).wrapContentSize())
+        Column(
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(top = 30.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Default.Edit,
+                contentDescription = "Draw",
+                tint = Color(22, 133, 169),
+                modifier = Modifier.width(60.dp).height(60.dp)
+            )
+            Text(
+                text = "Draw",
+                fontSize = 20.sp,
+                fontFamily = FontFamily.Serif,
+                modifier = Modifier.wrapContentSize().padding(top = 10.dp)
+            )
+        }
     }
 }
 
@@ -142,12 +189,26 @@ fun SettingsCard() {
         Modifier
             .fillMaxSize()
             .padding(10.dp),
-        elevation = 8.dp,
+        elevation = 6.dp,
         shape = RoundedCornerShape(10.dp)
     ) {
-//                Icon(imageVector = Icons.Default.Settings, contentDescription = "Settings")
-        Text(
-            text = "Settings",
-            modifier = Modifier.background(Color(194, 204, 208)).wrapContentSize())
+        Column(
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(top = 30.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Default.Settings,
+                contentDescription = "Settings",
+                tint = Color(22, 133, 169),
+                modifier = Modifier.width(60.dp).height(60.dp)
+            )
+            Text(
+                text = "Settings",
+                fontSize = 20.sp,
+                fontFamily = FontFamily.Serif,
+                modifier = Modifier.wrapContentSize().padding(top = 10.dp)
+            )
+        }
     }
 }
