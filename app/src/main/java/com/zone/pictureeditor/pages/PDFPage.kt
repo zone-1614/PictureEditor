aec3e37a649a6cbc09ba8d36f5d84e698a364ea3
Zone <2493920954@qq.com>
2022-10-20 00:55:09 +0800

    PDF页面 选择图片

--- app/src/main/java/com/zone/pictureeditor/pages/PDFPage.kt
+++ app/src/main/java/com/zone/pictureeditor/pages/PDFPage.kt
@@ -1,23 +1,83 @@
 package com.zone.pictureeditor.pages
 
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.util.Log
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.zone.pictureeditor.ui.theme.AppColor
+import com.zone.pictureeditor.util.PEApplication
 import com.zone.pictureeditor.util.Router
 import com.zone.pictureeditor.util.toast
+import com.zone.pictureeditor.vm.PDFViewModel
 
 @Composable
-fun PDFPage(navController: NavHostController) {
+fun PDFPage(
+    navController: NavHostController,
+    vm: PDFViewModel = viewModel()
+) {
+    val galleryLauncher = rememberLauncherForActivityResult(
+        ActivityResultContracts.GetMultipleContents()) { uriList ->
+        // 获得图片后添加到 viewModel 中
+        uriList.forEach { vm.imageUriList.add(it) }
+        Log.d("PE", uriList.toString())
+        "选择成功".toast()
+    }
     Scaffold(
         topBar = { PDFTopBar(navController = navController) },
-        floatingActionButton = { PDFFloatingActionButton() }
+        backgroundColor = AppColor.Background ,
+        floatingActionButton = { PDFFloatingActionButton(onClick = {
+            galleryLauncher.launch("image/*")
+        }) }
     ) {
-        Text(text = "This is Convert To PDF Page")
+        LazyColumn(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            items(vm.imageUriList.size) { idx ->
+                val source = ImageDecoder.createSource(PEApplication.context.contentResolver, vm.imageUriList[idx])
+                val bitmap = remember { mutableStateOf<Bitmap?>(null) }
+                bitmap.value = ImageDecoder.decodeBitmap(source)
+                bitmap.value?.let { btm ->
+                    Card(modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 40.dp, end = 40.dp, top = 20.dp)
+                        .height(200.dp)
+                    ) {
+                        Image(bitmap = btm.asImageBitmap(), contentDescription = "bitmap")
+                    }
+                }
+            }
+            if (vm.imageUriList.isNotEmpty()) {
+                item {
+                    Button(
+                        onClick = {
+                            // TODO: Convert To PDF
+                            "convert".toast()
+                        },
+                        modifier = Modifier.padding(top = 20.dp, bottom = 20.dp)
+                    ) {
+                        Text(text = "Convert")
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -31,13 +91,13 @@ fun PDFTopBar(navController: NavHostController) = TopAppBar(
             Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "ArrowBack")
         }
     },
-    backgroundColor = AppColor.MainColor
+    backgroundColor = AppColor.Background
 )
 
 @Composable
-fun PDFFloatingActionButton() = FloatingActionButton(
+fun PDFFloatingActionButton(onClick: () -> Unit) = FloatingActionButton(
     onClick = {
-        "click".toast()
+        onClick()
     },
     backgroundColor = AppColor.MainColor
 ) {
---
+++ app/src/main/java/com/zone/pictureeditor/vm/PDFViewModel.kt
@@ -0,0 +1,13 @@
+package com.zone.pictureeditor.vm
+
+import android.net.Uri
+import androidx.compose.runtime.mutableStateListOf
+import androidx.lifecycle.ViewModel
+
+class PDFViewModel : ViewModel() {
+    var imageUriList = mutableStateListOf<Uri>()
+
+    fun addPicture() {
+
+    }
+}
