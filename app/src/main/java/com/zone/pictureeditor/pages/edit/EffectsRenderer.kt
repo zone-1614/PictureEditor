fd4e0d6a6f5b32a177cf429c149d646822426ada
Zone <2493920954@qq.com>
2022-11-26 12:25:15 +0800

    edit界面 gles相关(还不能运行)

--- app/src/main/java/com/zone/pictureeditor/pages/edit/EffectsRenderer.kt
+++ app/src/main/java/com/zone/pictureeditor/pages/edit/EffectsRenderer.kt
@@ -55,8 +55,8 @@ class EffectsRenderer(): GLSurfaceView.Renderer {
      */
     private fun generateSquare(arr : FloatArray) {
         GLES20.glGenTextures(2, textures, 0)
-//        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textures[0])
-        GLES20.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, textures[0])
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textures[0])
+//        GLES20.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, textures[0])
         GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR)
         GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR)
         GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_S, GLES20.GL_CLAMP_TO_EDGE)
